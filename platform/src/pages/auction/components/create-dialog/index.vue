6737402e35af2202625e706ad576b71031702c7b
xujinxing <xujinxing@zuoyebang.com>
2024-06-11 17:08:51 +0800

    query

--- platform/src/pages/auction/components/create-dialog/index.vue
+++ platform/src/pages/auction/components/create-dialog/index.vue
@@ -108,8 +108,8 @@ const confirm = async () => {
 }
 const getZhuanyongQuery = (value: string, key: string) => {
   const map: any = {
-    diqu: `MATCH (a:geo_p) WHERE a.value = "${value}" RETURN a`,
-    hangye: `MATCH (a:hy) WHERE a.value = "${value}" RETURN a`,
+    diqu: `MATCH (a:geo_org) WHERE a.value CONTAINS "${value}" RETURN a`,
+    hangye: `MATCH (a:hy)-[r]-(b:geo_org) WHERE a.value="${value}" RETURN a,r,b LIMIT 50`,
     jigou: `MATCH (a:geo_org) WHERE a.value = "${value}" RETURN a`
   }
   return map[key]
